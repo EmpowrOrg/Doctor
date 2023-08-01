6ef9b8043aa69b7bb7d7ba645b286562761ee307
adevezin <adrianldevezin@mailfence.com>
2023-08-01 13:34:00 -0400

    Add unit testing to CSharpExecutor

--- executor/src/main/kotlin/org/empowrco/doctor/executor/CSharpExecutor.kt
+++ executor/src/main/kotlin/org/empowrco/doctor/executor/CSharpExecutor.kt
@@ -1,5 +1,6 @@
 package org.empowrco.doctor.executor
 
+import io.ktor.utils.io.core.use
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.empowrco.doctor.command.CommandResponse
@@ -9,6 +10,7 @@ import org.empowrco.doctor.models.ExecutorResponse
 import org.empowrco.doctor.models.Success
 import org.empowrco.doctor.utils.files.FileUtil
 import java.io.File
+import java.io.FileWriter
 
 class CSharpExecutor(private val commander: Commander, private val fileUtil: FileUtil) : Executor() {
     override val handledLanguages: Set<String> = setOf("text/x-csharp", "csharp", "c#")
@@ -45,6 +47,55 @@ class CSharpExecutor(private val commander: Commander, private val fileUtil: Fil
     }
 
     override suspend fun test(code: String, unitTests: String): ExecutorResponse {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            val tempFolder = fileUtil.createTempDirectory()
+            return@withContext try {
+                val nameRegex = "class\\s(.*)?\\s".toRegex()
+                val testName =
+                    nameRegex.find(unitTests)?.groupValues?.lastOrNull()?.removeSuffix(":")?.removeSuffix("Tests")
+                        ?: return@withContext Error("Unit tests were not wrapped in a valid class")
+                val namespaceRegex = "namespace\\s(.*);".toRegex()
+                val namespace = namespaceRegex.find(unitTests)?.groupValues?.lastOrNull()
+                    ?: namespaceRegex.find(code)?.groupValues?.lastOrNull() ?: "XunitTesting"
+                val createTestResult =
+                    commander.execute("dotnet new xunit -n $namespace", tempFolder)
+                val testFolder = File(tempFolder, "/$namespace")
+                val codeFile = testFolder.listFiles()?.firstOrNull {
+                    it.name.contains("Usings", ignoreCase = true) && it.name.endsWith(
+                        ".cs",
+                        ignoreCase = true
+                    )
+                }
+                    ?: return@withContext Error(
+                        "Error finding source file for Test $testName"
+                    )
+                FileWriter(codeFile).use {
+                    it.write("")
+                    it.appendLine(code)
+                }
+                val testsFile = testFolder.listFiles()?.firstOrNull {
+                    it.name.endsWith(".cs", ignoreCase = true) && it.name.contains("test", ignoreCase = true)
+                } ?: return@withContext Error(
+                    "Error finding test file for Test $testName"
+                )
+                FileWriter(testsFile).use {
+                    it.write("")
+                    it.write(unitTests)
+                }
+                if (createTestResult is CommandResponse.Error) {
+                    return@withContext Error(createTestResult.output)
+                }
+                val executeTestResult = commander.execute("dotnet test", testFolder)
+                if (executeTestResult is CommandResponse.Success) {
+                    return@withContext Success(executeTestResult.output, false)
+                } else {
+                    return@withContext Error(executeTestResult.output)
+                }
+            } catch (ex: Exception) {
+                Error(ex.message ?: "")
+            } finally {
+                fileUtil.deleteFiles(tempFolder)
+            }
+        }
     }
 }
