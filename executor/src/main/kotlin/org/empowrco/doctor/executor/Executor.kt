4cbb2d0c9bb857e065377b713886299b76ff8d4d
adevezin <adrianldevezin@mailfence.com>
2023-08-01 13:33:48 -0400

    Format executor

--- executor/src/main/kotlin/org/empowrco/doctor/executor/Executor.kt
+++ executor/src/main/kotlin/org/empowrco/doctor/executor/Executor.kt
@@ -3,12 +3,12 @@ package org.empowrco.doctor.executor
 import org.empowrco.doctor.models.ExecutorResponse
 
 
- abstract class Executor {
-     abstract val handledLanguages: Set<String>
-     abstract suspend fun execute(code: String): ExecutorResponse
-     abstract suspend fun test(code: String, unitTests: String): ExecutorResponse
-     fun canHandle(language: String): Boolean {
-         return handledLanguages.contains(language)
-     }
- }
+abstract class Executor {
+    abstract val handledLanguages: Set<String>
+    abstract suspend fun execute(code: String): ExecutorResponse
+    abstract suspend fun test(code: String, unitTests: String): ExecutorResponse
+    fun canHandle(language: String): Boolean {
+        return handledLanguages.contains(language)
+    }
+}
 
